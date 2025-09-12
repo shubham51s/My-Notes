02e49f1705d562816b9637bd31e1b57a03780e55
Shubham Solat <shubhamsolat51@gmail.com>
2025-09-12 11:56:26 +0530

    system design notes

---
+++ systemDesign.js
@@ -0,0 +1,43 @@
+/* 
+    # System Design:
+        -> High level of planning & structuring of how web application works as a whole.
+        -> System Design is the practice of planning and defining the architecture, components, data flow, and technologies of a software application before writing the code
+
+            * key aspects of System Design
+            1. Architecture
+            2. Frontend Design
+            3. Backend Design
+            4. Database Design
+            5. Scalabilitys
+            6. Data Flow
+            7. Security
+    
+
+    # Frontend Architecture:
+
+        Frontend architecture is the structure and design of everything that runs on user's browser. It's concerned with the user interface (UI) & user experience (UX).
+         
+        Primary goal: to create fast, reliable, scalable & maintanable application that provides the excellent experience for the user
+        
+        Key concepts: 
+            1. UI components & structure
+            2. State management
+            3. Routing
+            4. Build & deployment
+            5. Performance optimization
+
+    
+    # Backend Architecture: 
+        Backend architecture is the structure and design of everything that runs on the server. It's concerned with business logic, data processing, security, and integration.
+
+        Primary goal: to reliably serve data, enforce business rules, handle security, and communicate with other services, all while being scalable and efficient
+        
+        Key concepts:
+            1. Application logic
+            2. Data persistence & databases
+            3. API design
+            4. Authentication & authorization
+            5. Caching
+
+
+*/
