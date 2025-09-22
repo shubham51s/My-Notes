02074003e70c410a58923c1a3ce146a0dd8322a2
Shubham Solat <shubhamsolat51@gmail.com>
2025-09-22 12:04:58 +0530

    react notes page created

---
+++ Frontend/react.js
@@ -0,0 +1,15 @@
+/*
+    # React hooks:
+
+
+    # Custom hooks
+        - A custom hook is a javascript function that starts with use & internally calls other hooks like useState, useEffect, or useContext. It allows developers to extract reusable logic, keeping components clean and modular.
+        - Steps to create Custom Hook: 
+            1. Define a Function That Starts with use
+            2. Use React's Built-in Hooks Inside Your Custom Hook
+            3. Return Necessary Values
+        - When to Use Custom Hooks:
+            1. We need to reuse logic across multiple components.
+            2. We want to improve readability and maintainability by keeping component logic clean.
+
+*/
